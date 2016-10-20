c596ed8e3c11ac16870a64cbac135fcb3535bac7
bilinskyy <bilinskyypp@gmail.com>
2016-10-20 16:50:45 +0300

    changed paths to train/test files

---
+++ DarknetTrainer/src/constants.h
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+// Applied only for YOLO model!
+#define CLASS_NAMES {"person"}
+#define NUM_CLASSES 1
+#define TRAIN_IMAGES_PATHS_FILE "data/train.txt"
+#define TEST_IMAGES_PATHS_FILE  "data/test.txt"
+#define BACKUP_DIR "backup/"
+
+#endif //CONSTANTS_H
